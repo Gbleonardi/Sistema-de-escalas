5f969ab14cd7a42010639845dc483e846b405d75
Gabriel Leonardi <gabrielleonardi@GabrielleonariDb11.Gabriel>
2022-04-22 05:41:54 -0300

    validação usuario e senha

---
+++ js/logado.js
@@ -0,0 +1,20 @@
+// Função exibir nome
+let userLogado = JSON.parse(localStorage.getItem("userLogado"))
+let logado = document.querySelector("#Logado")
+
+logado.innerHTML = `Olá  ${userLogado.nome}`
+
+
+// Em caso de tentatifa de login diretamente pelo link, exibe o alerta
+if(localStorage.getItem("token") == null){
+    alert("voce precisa estar logado para acessar essa página")
+    window.location.href = "index.html"
+}
+
+// botção log out
+function sair() {
+    localStorage.removeItem("token")
+    localStorage.removeItem("userLogado")
+    window.location.href = "index.html"
+    
+}
